96d22908ac2b30d3deb87dd7e935fd7dea3ee152
zhongguancunlujiang <632450286@qq.com>
2020-10-21 22:34:22 +0800

    测试冲突解决

--- PythonCommon.py
+++ PythonCommon.py
@@ -1,8 +1,9 @@
-#此文件主要记录一些python相关常考语法特性
+﻿#此文件主要记录一些python相关常考语法特性
 #一、python各种数据类型的推导式
 
 #1、字典推导式
 #a、常考点：一句话实现字典的kev、value交换
 temp_dict = {"cheng": 100, "du":200, "ok":300}
+#采用推导式即可
 temp_dict = { value:key for key, value in temp_dict.items() }
 print(temp_dict)
